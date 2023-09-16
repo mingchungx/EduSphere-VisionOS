85283d3cc9c569aa0e4fa140ebe2c010309eaa89
Mingchung Xia <mingchung.xia@gmail.com>
2023-09-16 10:35:01 -0400

    Initial Commit

---
+++ EduSphere/EduSphereApp.swift
@@ -0,0 +1,21 @@
+//
+//  EduSphereApp.swift
+//  EduSphere
+//
+//  Created by Mingchung Xia on 2023-09-16.
+//
+
+import SwiftUI
+
+@main
+struct EduSphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }.immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
